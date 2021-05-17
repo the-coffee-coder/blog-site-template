bab5d23f00e056b72e86fc28100ae8537db5bedf
Brandon Harper <80657696+the-coffee-coder@users.noreply.github.com>
2021-05-16 21:13:27 -0500

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,17 @@
+<footer class="footer text-center py-2 theme-bg-dark">
+		   
+    <p class="copyright"><a href="http://www.brandonharper.me">Brandon Harper</a></p>
+    
+    <?php
+        dynamic_sidebar('footer-1');
+    ?>
+</footer>
+
+</div>
+
+<?php
+    wp_footer();
+?>
+
+</body>
+</html> 
